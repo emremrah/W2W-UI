e82ca48bee9f3b1de43a27d530bd623182f9bd00
Emre Emrah <emre95@gmail.com>
2023-12-30 22:43:41 +0300

    Add cover image url

--- src/app/movie-list/movie/movie.model.ts
+++ src/app/movie-list/movie/movie.model.ts
@@ -2,13 +2,9 @@ export class Movie {
   constructor(
     public id: string,
     public title: string,
-    public plot: string,
-    public genres: Array<string>,
-    public rating: number
-  ) {
-    (this.title = title),
-      (this.plot = plot),
-      (this.genres = genres),
-      (this.rating = rating);
-  }
+    public plot?: string,
+    public rating?: number,
+    public genres?: Array<string>,
+    public image_url?: string
+  ) {}
 }
